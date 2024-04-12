f15835deb20499f3852acd6248114dd232d2c3f2
minhhieu3110 <phamminhhieu599@gmail.com>
2024-04-12 18:40:03 +0700

    commit lan 2: thay doi dieu kien

--- hinhtron.js
+++ hinhtron.js
@@ -1,10 +1,20 @@
 function dt(){
     let bk = +document.getElementById('r').value;
-    let dtich = 3.14*bk*bk;
-    document.getElementById('dt').innerHTML= "Diện tích: "+ dtich;
+    if(bk < 0){
+        document.getElementById('dt').innerHTML='Bán kính hình tròn phải lớn hơn 0';
+    }else {
+        let dtich = 3.14*bk*bk;
+        document.getElementById('dt').innerHTML= "Diện tích: "+ dtich;
+    }
+
 }
 function cv(){
     let bk = +document.getElementById('r').value;
-    let cvi = 3.14*2*bk;
-    document.getElementById('cv').innerHTML="Chu Vi: "+ cvi;
+    if(bk < 0){
+        document.getElementById('cv').innerHTML='Bán kính hình tròn phải lớn hơn 0';
+    }else {
+        let cvi = 3.14*2*bk;
+        document.getElementById('cv').innerHTML="Chu Vi: "+ cvi;
+    }
+
 }
